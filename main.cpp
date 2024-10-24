a5fe636e27c645b94ed6ee32df2895769ffa943b
Sam Armstrong <sjarmstrong@nic.edu>
2024-10-24 16:27:36 -0700

    added say function

--- main.cpp
+++ main.cpp
@@ -2,12 +2,21 @@
 
 using namespace std;
 
+void say(string s);
+
 int main()
 {
 
     cout << "test" << endl;
     cout << "hello world" << endl;
+
+    say("hi");
     
 
     return 0;
+}
+
+void say(string s)
+{
+    cout << s << endl;
 }
