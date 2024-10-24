5d8e9f3ecb7f98bc3857e1006457f1ae223d917a
Sam Armstrong <sjarmstrong@nic.edu>
2024-10-24 16:24:41 -0700

    created add function

--- main.cpp
+++ main.cpp
@@ -2,12 +2,19 @@
 
 using namespace std;
 
+int add(int a, int b);
+
 int main()
 {
 
     cout << "test" << endl;
     cout << "hello world" << endl;
-    
+    cout << "Sum of 3 and 4 is: " << add(3,4) << endl;
 
     return 0;
+}
+
+int add(int a, int b)
+{
+    return a + b;
 }
